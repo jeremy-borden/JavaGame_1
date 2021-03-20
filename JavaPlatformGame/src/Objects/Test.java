9471dc3bac095eac943acc1b055ab7bbc023c847
jeremy-borden <jeremygborden@gmail.com>
2021-03-20 15:55:08 -0400

    o

--- JavaPlatformGame/src/Objects/Test.java
+++ JavaPlatformGame/src/Objects/Test.java
@@ -1,5 +1,59 @@
 package Objects;
 
-public class Test {
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+import Framework.GameObject;
+import Framework.ObjectId;
+
+public class Test extends GameObject{
+
+    public Test(float x, float y, ObjectId id) {
+        super(x, y, id);
+    }
+
+    public void tick(LinkedList<GameObject> object) {
+        
+    }
+
+    public void render(Graphics g) {
+        
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getVelX() {
+        return velX;
+    }
+
+    public float getVelY() {
+        return velY;
+    }
+
+    public void setVelX(float velX) {
+        this.velX = velX;
+    }
+
+    public void setVelY(float velY) {
+        this.velY = velY;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
     
 }
