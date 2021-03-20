e0ac4273e7005d6f0ebc2baa86dd61956864bfa7
jeremy-borden <jeremygborden@gmail.com>
2021-03-20 17:00:19 -0400

    e

---
+++ JavaPlatformGame/src/Framework/KeyInput.java
@@ -0,0 +1,23 @@
+package Framework;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import Window.Game;
+
+public class KeyInput extends KeyAdapter {
+
+    Game game;
+
+    public KeyInput(Game game) {
+        this.game = game;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        game.keyPressed(e);
+    }
+
+    public void keyReleased(KeyEvent e) {
+        game.keyReleased(e);
+    }
+}
--- JavaPlatformGame/src/Objects/Player.java
+++ JavaPlatformGame/src/Objects/Player.java
@@ -1,6 +1,7 @@
 package Objects;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Color;
 import java.util.LinkedList;
@@ -10,7 +11,7 @@ import Framework.ObjectId;
 
 public class Player extends GameObject {
 
-    private float width = 32, height = 64;
+    private float width = 48, height = 96;
     private float gravity = 0.1f;
     private final float MAX_SPEED = 10;
 
@@ -20,7 +21,7 @@ public class Player extends GameObject {
 
     public void tick(LinkedList<GameObject> object) {
         x+=velX;
-        y+=velY;
+        //y+=velY;
 
         if(falling || jumping){
             velY += gravity;
@@ -34,10 +35,30 @@ public class Player extends GameObject {
     public void render(Graphics g) {
         g.setColor(Color.blue);
         g.fillRect((int) x, (int) y, (int) width, (int) height);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g.setColor(Color.red);
+        g2d.draw(getBounds());
+        g2d.draw(getBoundsRight());
+        g2d.draw(getBoundsLeft());
+        g2d.draw(getBoundsTop());
     }
 
     public Rectangle getBounds() {
-        return new Rectangle((int) x, (int) y, (int) width, (int) height);
+        return new Rectangle((int) (x + (width/2) - (width/4)), (int) (y + (height/2)), (int) width/2, (int) height/2);
+    }
+
+    public Rectangle getBoundsTop() {
+        return new Rectangle((int) (x + (width/2) - (width/4)), (int) y, (int) width/2, (int) height/2 );
+    }
+
+    public Rectangle getBoundsRight() {
+        return new Rectangle((int) (x+width-5), (int) y+5, 5, (int) height-10);
+    }
+
+    public Rectangle getBoundsLeft() {
+        return new Rectangle((int) x, (int) y+5, 5, (int) height-10);
     }
 
 }
