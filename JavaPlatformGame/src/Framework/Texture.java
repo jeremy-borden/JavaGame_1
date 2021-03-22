ce2be01d78ba8a1dfed11d188690979eafc5e317
jeremy-borden <jeremygborden@gmail.com>
2021-03-22 18:48:12 -0400

    d

--- JavaPlatformGame/src/Framework/Texture.java
+++ JavaPlatformGame/src/Framework/Texture.java
@@ -17,12 +17,6 @@ public class Texture {
         BufferedImageLoader loader = new BufferedImageLoader();
         block_sheet = loader.loadImage("/blockSheet.png");
         player_sheet = loader.loadImage("/player_sheet.png");
-        // try{
-        // block_sheet = loader.loadImage("/block_sheet.png");
-        // player_sheet = loader.loadImage("/player_sheet.png");
-        // }catch(Exception e){
-        // e.printStackTrace();
-        // }
         bs = new SpriteSheet(block_sheet);
         ps = new SpriteSheet(player_sheet);
         getTextures();
